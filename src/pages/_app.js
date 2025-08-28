aaff39a6dd08859eb23d9d42e75920afd2bc8277
kaushal209 <kaushal2092003@gmail.com>
2025-08-28 13:51:05 +0530

    setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <main className="main_wrapper">
+        {/* { !is404Page && <Header /> } */}
+        <Component {...pageProps} />
+        {/* { !is404Page && <Footer /> } */}
+      </main>
+    </>
+  )
 }
