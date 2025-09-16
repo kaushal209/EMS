55fc4c28f31f10f041413630ec9fbe5514ff5f32
kaushal209 <kaushal2092003@gmail.com>
2025-09-16 19:04:29 +0530

    header structure

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,21 +1,62 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 import crossIcon from '../../../public/cross-icon.svg';
+import estateinLogo from '../../../public/estatein-logo.svg';
+
 
 const Header = () => {
+
+    const [showDiscover, setShowDiscover] = useState(true);
+    const discoverHandler = (e) => {
+		e.preventDefault();
+		setShowDiscover(!showDiscover);
+	}
+
     return (
         <>
             <header className="header">
-                <div className="discover">
-                    <div className="discover-section">
-                        <div className="left-part">
-                            <span>✨Discover Your Dream Property with Estatein</span>
-                            <div className="learn-btn">
-                                <Link href="#">Learn More</Link>
+                {
+                    showDiscover && (
+                        <div className="discover">
+                            <div className="discover-section">
+                                <div className="left-part">
+                                    <span>✨Discover Your Dream Property with Estatein</span>
+                                    <div className="learn-btn">
+                                        <Link href="#">Learn More</Link>
+                                    </div>
+                                </div>
+                                <div className="right-part">
+                                    <Link href="#" onClick={(e) => discoverHandler(e)}>
+                                        <i><Image src={crossIcon} alt="cross" width="24" height="24" /></i>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                        <div className="right-part">
-                            <Link href="#"><i><Image src={crossIcon} alt="cross" width="24" height="24" /></i></Link>
+                    )
+                }
+                <div className="container">
+                    <div className="main_header">
+                        <div className="logo">
+                            <Link href="/"><Image src={estateinLogo} alt="logo" width="160" height="48" /></Link>
+                        </div>
+                        <div className="navbar_part">
+                            <nav className="navbar">
+                                <ul>
+                                    <li><Link href="/">Home</Link></li>
+                                    <li><Link href="/">About Us</Link></li>
+                                    <li><Link href="/">Properties</Link></li>
+                                    <li><Link href="/">Services</Link></li>
+                                </ul>
+                            </nav>
+                            <div className="contact-btn">
+                                <Link href="/">Contact Us</Link>
+                            </div>
+                        </div>
+                        <div className="menu-btn">
+                            <Link href="#">
+                                <span></span>
+                            </Link>
                         </div>
                     </div>
                 </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/font.css';
 import "@/styles/globals.css";
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import { ReactLenis, useLenis } from 'lenis/react';
 
 export default function App({ Component, pageProps }) {
+
+  const lenis = useLenis(({ scroll }) => {
+		// called every scroll
+	});
+
   return (
     <>
-      <main className="main_wrapper">
-        {/* { !is404Page && <Header /> } */}
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-        {/* { !is404Page && <Footer /> } */}
-      </main>
+      <ReactLenis root>
+        <main className="main_wrapper">
+          {/* { !is404Page && <Header /> } */}
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+          {/* { !is404Page && <Footer /> } */}
+        </main>
+      </ReactLenis>
     </>
   )
 }
